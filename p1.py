02eadc37ff8902acce1133379917b07f79338dd7
Γιώργος Ρωμαίος <george_romaios@hotmail.com>
2024-10-28 23:55:19 +0200

    using an activation function

--- p1.py
+++ p1.py
@@ -1,10 +1,10 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data  # See for code: https://gist.github.com/Sentdex/454cb20ec5acf0e76ee8ab8448e6266c
 
-np.random.seed(0)
+nnfs.init()
 
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
+X, y = spiral_data(100, 3)
 
 
 class Layer_Dense:
@@ -14,56 +14,17 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-layer1 = Layer_Dense(4,5)
-layer2 = Layer_Dense(5,2)
-layer3 = Layer_Dense(2,5)
-layer4 = Layer_Dense(5,5)
 
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+layer1 = Layer_Dense(2,5)
+activation1 = Activation_ReLU()
 
 layer1.forward(X)
-layer2.forward(layer1.output)
-layer3.forward(layer2.output)
-layer4.forward(layer3.output)
 
-
-print(layer1.output,"\n")
-print(layer2.output,"\n")
-print(layer3.output,"\n")
-print(layer4.output,"\n")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-# inputs = [1,2,3,2.5]
-#
-# weights = [[0.2,0.8,-0.5,1.0],[0.5,-0.91,0.26,-0.5],[-0.26,-0.27,0.17,0.87]]
-# biases = [2, 3, 0.5]
-#
-# some_value = -0.5
-# weight = 0.7
-# bias =0.7
-#
-
-# layer_outputs = []
-#
-# for neuron_weights, neuron_bias in zip (weights, biases):
-#     neuron_output =0
-#     for n_input, weight in zip(inputs,neuron_weights):
-#         neuron_output += n_input*weight
-#
-#     neuron_output += neuron_bias
-#     layer_outputs.append(neuron_output)
-#
-# print (layer_outputs)
+#print(layer1.output)
+activation1.forward(layer1.output)
+print(activation1.output)
